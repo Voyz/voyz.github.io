2930448f9f118417ecbab099e665f3509ae1322e
voyz <voy1982@yahoo.co.uk>
2025-03-17 21:13:33 +0100

    update

--- src/scripts.js
+++ src/scripts.js
@@ -12,31 +12,38 @@ async function loadComponent(id, file, callback) {
 }
 
 // Function to rebind FAQ event listeners
-function initializeFAQ() {
-	document.querySelectorAll('.faq-question').forEach(item => {
-		item.addEventListener('click', () => {
-			const answer = item.nextElementSibling;
-			answer.classList.toggle('hidden');
-		});
-	});
-
-}
+// function initializeFAQ() {
+// 	document.querySelectorAll('.faq-question').forEach(item => {
+// 		item.addEventListener('click', () => {
+// 			const answer = item.nextElementSibling;
+// 			answer.classList.toggle('hidden');
+// 		});
+// 	});
+//
+// }
 
 
 async function loadAll() {
 	await Promise.all([
-		loadComponent("header", "components/header.html"),
+		// loadComponent("header", "components/header.html"),
 		loadComponent("services", "components/services.html"),
 		loadComponent("why_work", "components/why_work.html"),
 		loadComponent("testimonials", "components/testimonials.html"),
 		loadComponent("my_typical", "components/my_typical.html"),
-		loadComponent("faq", "components/faq.html", initializeFAQ)
+		loadComponent("faq", "components/faq.html")
 	]);
 
-	lucide.createIcons(); // Initialize Lucide Icons
+	try {
+		lucide.createIcons(); // Initialize Lucide Icons
+	} catch (error) {
+		console.error("Lucide Icon Error:", error);
+	}
 
-	// Now remove the hidden class AFTER everything has loaded
 	const loadable = document.getElementById("loadable");
+	loadable.classList.add("transition-opacity");  // Temporarily hide
+	loadable.classList.add("duration-700");  // Temporarily hide
+	loadable.classList.add("ease-in-out");  // Temporarily hide
+	// Now remove the hidden class AFTER everything has loaded
 	loadable.classList.remove("opacity-0"); // Start fade-in
 }
 
